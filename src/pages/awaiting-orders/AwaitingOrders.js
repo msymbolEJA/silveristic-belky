253f9053e558670a74a15c985f1f2b1fd81eaa37
ed ben <edwardbenedict92@gmail.com>
2021-06-02 17:25:50 +0300

    feat: add awaiting-orders table

--- src/pages/awaiting-orders/AwaitingOrders.js
+++ src/pages/awaiting-orders/AwaitingOrders.js
@@ -1,9 +1,17 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: "50px",
+    // marginTop: "50px",
     marginLeft: "10px",
     display: "flex",
     flexDirection: "column",
@@ -29,15 +37,52 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     lineHeight: "1.5rem",
   },
+  tContainer: {
+    marginRight: "5px",
+    width: "calc(100vw - 20px)",
+  },
+  thead: {
+    backgroundColor: "#6495ED",
+  },
+  header: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    marginBottom: "0px",
+  },
+  found: {
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+  },
 }));
+
+const rows = [
+  {
+    receiptNumber: 132427,
+    no: 2071615284,
+    date: "06/02/21 08:31:39",
+    adet: "1/1/1",
+    systemDate: "06/02/21 08:43:00",
+    buyer: "Pamela Higgins",
+    supplier: "beyazit",
+    type: "Kolye Tasli",
+    length: "45cm",
+    color: "GOLD",
+    qty: "3 Tas",
+    size: "-",
+    start: "ortada",
+    space: "-",
+    explanation: "Emerald Alexandrite Emerald",
+    internalNote: "",
+  },
+];
 
 const AwaitingOrders = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <h2>Bekleyen Siparişleriniz</h2>
-      <h3>{"0"} result found!</h3>
+      <p className={classes.header}>Bekleyen Siparişleriniz</p>
+      <p className={classes.found}>{"0"} result found!</p>
       <div className={classes.btnGroup}>
         <Button variant="contained" className={classes.button}>
           All
@@ -48,6 +93,57 @@ const AwaitingOrders = () => {
         <Button variant="contained" className={classes.button}>
           International
         </Button>
+      </div>
+      <div className={classes.paper}>
+        <TableContainer component={Paper} className={classes.tContainer}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead className={classes.thead}>
+              <TableRow>
+                <TableCell>Sip.No / No</TableCell>
+                <TableCell align="center">Tarih / Adet</TableCell>
+                <TableCell align="center">Sistem Tarihi</TableCell>
+                <TableCell align="center">Buyer</TableCell>
+                <TableCell align="center">Supplier</TableCell>
+                <TableCell align="center">Tip</TableCell>
+                <TableCell align="center">Uzunluk</TableCell>
+                <TableCell align="center">Renk</TableCell>
+                <TableCell align="center">Adet</TableCell>
+                <TableCell align="center">Boyut</TableCell>
+                <TableCell align="center">Başlangıç</TableCell>
+                <TableCell align="center">Boşluk</TableCell>
+                <TableCell align="center">Açıklama</TableCell>
+                <TableCell align="center">Internal Note</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.receiptNumber}>
+                  <TableCell component="th" scope="row" align="center">
+                    <Link to={`/order?order=${row.receiptNumber}`}>
+                      {row.receiptNumber}
+                    </Link>
+                    <br /> {row.no}
+                  </TableCell>
+                  <TableCell align="center">
+                    {row.date} <br /> {row.adet}
+                  </TableCell>
+                  <TableCell align="center">{row.systemDate}</TableCell>
+                  <TableCell align="center">{row.buyer}</TableCell>
+                  <TableCell align="center">{row.supplier}</TableCell>
+                  <TableCell align="center">{row.type}</TableCell>
+                  <TableCell align="center">{row.length}</TableCell>
+                  <TableCell align="center">{row.color}</TableCell>
+                  <TableCell align="center">{row.qty}</TableCell>
+                  <TableCell align="center">{row.size}</TableCell>
+                  <TableCell align="center">{row.start}</TableCell>
+                  <TableCell align="center">{row.space}</TableCell>
+                  <TableCell align="center">{row.explanation}</TableCell>
+                  <TableCell align="center">{row.internalNote}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
       <div className={classes.btnGroup}>
         <Button variant="contained" className={classes.button}>
