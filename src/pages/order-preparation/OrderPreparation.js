7b4366dfe2cbed3c25e4ebd96b0598827183d29c
edwardBenedict <edwardbenedict92@gmail.com>
2021-06-15 11:09:25 +0300

    fix: internalnote on preparation page

--- src/pages/order-preparation/OrderPreparation.js
+++ src/pages/order-preparation/OrderPreparation.js
@@ -9,7 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import { editableTableColumns } from "../../helper/Constants";
 import { getData, putData } from "../../helper/PostData";
 import moment from "moment";
-import EditableCell from '../../components/newitems/EditableCell'
+import EditableCell from "../../components/newitems/EditableCell";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const BASE_URL_MAPPING = process.env.REACT_APP_BASE_URL_MAPPING;
@@ -193,39 +193,39 @@ const AwaitingOrders = () => {
                         className={classes.tableCell}
                         align="center"
                       >
-                        {
-                          item?.objKey === "approved" ? (
-                            <label className="container">
-                              <input
-                                type="checkbox"
-                                className={classes.checkbox}
-                                defaultChecked={row[item?.objKey]}
-                                onClick={(e) => handleReady(e, row.id)}
-                                name="approved"
-                              />
-                            </label>
-                          ) : item?.objKey === "personalization" &&
-                            row[item?.objKey] === null || row[item?.objKey] === ""  ? (
-                            "None"
-                          ) : item?.objKey === "status" ? (
-                            <select
-                              name="status"
-                              id="status"
-                              className={classes.select}
-                              value={row[item?.objKey]}
-                              onChange={(e) => handleOptionChange(e, row.id)}
-                            >
-                              <option value="awaiting">awaiting</option>
-                              <option value="pending">pending</option>
-                              <option value="in_progress">processing</option>
-                              <option value="ready">ready</option>
-                              <option value="in_transit">in_transit</option>
-                              <option value="repeat">repeat</option>
-                              <option value="shipped">shipped</option>
-                              <option value="cancelled">cancelled</option>
-                              <option value="follow_up">follow_up</option>
-                            </select>
-                          ) : item?.objKey === "supplier" ? (
+                        {item?.objKey === "approved" ? (
+                          <label className="container">
+                            <input
+                              type="checkbox"
+                              className={classes.checkbox}
+                              defaultChecked={row[item?.objKey]}
+                              onClick={(e) => handleReady(e, row.id)}
+                              name="approved"
+                            />
+                          </label>
+                        ) : item?.objKey === "personalization" &&
+                          (row[item?.objKey] === null ||
+                            row[item?.objKey] === "") ? (
+                          "None"
+                        ) : item?.objKey === "status" ? (
+                          <select
+                            name="status"
+                            id="status"
+                            className={classes.select}
+                            value={row[item?.objKey]}
+                            onChange={(e) => handleOptionChange(e, row.id)}
+                          >
+                            <option value="awaiting">awaiting</option>
+                            <option value="pending">pending</option>
+                            <option value="in_progress">processing</option>
+                            <option value="ready">ready</option>
+                            <option value="in_transit">in_transit</option>
+                            <option value="repeat">repeat</option>
+                            <option value="shipped">shipped</option>
+                            <option value="cancelled">cancelled</option>
+                            <option value="follow_up">follow_up</option>
+                          </select>
+                        ) : item?.objKey === "supplier" ? (
                           <select
                             name="supplier"
                             id="supplier"
@@ -237,32 +237,33 @@ const AwaitingOrders = () => {
                             <option value="beyazit">beyazit</option>
                             <option value="stok">stok</option>
                           </select>
-                        ) :
-                        item?.objKey === "created_date" ? 
-                        moment(row[item?.objKey]).format("MM-DD-YY HH:mm") === "Invalid date" ? (
-                          row[item?.objKey]
+                        ) : item?.objKey === "created_date" ? (
+                          moment(row[item?.objKey]).format("MM-DD-YY HH:mm") ===
+                          "Invalid date" ? (
+                            row[item?.objKey]
+                          ) : (
+                            moment
+                              .utc(row[item?.objKey])
+                              .local()
+                              .format("MM-DD-YY HH:mm")
+                          )
+                        ) : item?.objKey === "type" ||
+                          item?.objKey === "length" ||
+                          item?.objKey === "color" ||
+                          item?.objKey === "qty" ||
+                          item?.objKey === "size" ||
+                          item?.objKey === "start" ||
+                          item?.objKey === "explanation" ||
+                          item?.objKey === "space" ||
+                          item?.objKey === "note" ? (
+                          <EditableCell
+                            {...{
+                              row,
+                              name: item?.objKey,
+                              onChange,
+                            }}
+                          />
                         ) : (
-                          moment.utc(row[item?.objKey]).local().format("MM-DD-YY HH:mm")
-                        )
-                        : 
-                        item?.objKey === "type" ||
-                        item?.objKey === "length" ||
-                        item?.objKey === "color" ||
-                        item?.objKey === "qty" ||
-                        item?.objKey === "size" ||
-                        item?.objKey === "start" ||
-                        item?.objKey === "explanation" ||
-                        item?.objKey === "space" ||
-                        item?.objKey === "note" ? (
-                        <EditableCell
-                          {...{
-                            row,
-                            name: item?.objKey,
-                            onChange,
-                          }}
-                        />
-                      ) 
-                        : (
                           row[item?.objKey]
                         )}
                         {item?.objKey2 ? (
