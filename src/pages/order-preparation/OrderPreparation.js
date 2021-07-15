70fe828d8c5852ed948fbf4509554e707061f003
edwardBenedict <edwardbenedict92@gmail.com>
2021-07-15 22:06:48 +0300

    feat: add approve all button

--- src/pages/order-preparation/OrderPreparation.js
+++ src/pages/order-preparation/OrderPreparation.js
@@ -11,6 +11,10 @@ import { getData, putData } from "../../helper/PostData";
 import moment from "moment";
 import EditableCell from "../../components/newitems/EditableCell";
 import { Link } from "react-router-dom";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import { toastWarnNotify } from "../../components/otheritems/ToastNotify";
+import { postData } from "../../helper/PostData";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const BASE_URL_MAPPING = process.env.REACT_APP_BASE_URL_MAPPING;
@@ -105,13 +109,16 @@ const AwaitingOrders = () => {
   const classes = useStyles();
   const [rows, setRows] = useState([]);
   const [count, setCount] = useState();
+  const [selected, setSelected] = useState([]);
 
   const getOrders = () => {
-    getData(`${BASE_URL}etsy/orders/?status=pending`).then((response) => {
-      console.log(response.data);
-      setCount(response.data.count);
-      setRows(response.data.results);
-    });
+    getData(`${BASE_URL}etsy/orders/?status=pending&limit=100&offset=0`).then(
+      (response) => {
+        console.log(response.data);
+        setCount(response.data.count);
+        setRows(response.data.results);
+      }
+    );
   };
 
   useEffect(() => {
@@ -150,6 +157,50 @@ const AwaitingOrders = () => {
     [rows]
   );
 
+  const handleApproveSelected = () => {
+    postData(`${BASE_URL}etsy/approved_all/`, { ids: selected })
+      .then((res) => {
+        // toastWarnNotify("Selected 'PENDING' orders are approved");
+        getOrders();
+        setSelected([]);
+      })
+      .catch(({ response }) => {
+        toastWarnNotify(response?.data?.detail);
+      });
+  };
+
+  const handleCheckBoxClick = (event, id, row) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+    if (!(!!row.supplier && !!row.type && !!row.color && !!row.length)) return;
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected?.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows?.map((row) => {
+        if (!(!!row.supplier && !!row.type && !!row.color && !!row.length))
+          return null;
+        return row?.id;
+      });
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
   const handleOptionChange = (e, id) => {
     handleRowChange(id, { [e.target.name]: e.target.value });
   };
@@ -175,115 +226,186 @@ const AwaitingOrders = () => {
             <Table className={classes.table} aria-label="simple table">
               <TableHead className={classes.thead}>
                 <TableRow>
-                  {editableTableColumns?.map((item) => (
-                    <TableCell
-                      className={classes.tableCellHeader}
-                      align="center"
-                      key={item.id}
-                    >
-                      {item.name} {item?.name2 ? `/ ${item?.name2}` : null}
-                    </TableCell>
-                  ))}
+                  {editableTableColumns?.map((item) =>
+                    item?.objKey === "approved" ? (
+                      <TableCell
+                        align="center"
+                        style={{
+                          padding: 0,
+                          borderRight: "0.5px solid #E0E0E0",
+                        }}
+                      >
+                        <Button
+                          color="primary"
+                          variant="contained"
+                          onClick={handleApproveSelected}
+                          className={classes.approveButton}
+                          disabled={!selected?.length}
+                        >
+                          Approve (
+                          {
+                            selected.filter(function (el) {
+                              return el != null;
+                            })?.length
+                          }
+                          )
+                        </Button>
+                        <br />
+                        <Checkbox
+                          indeterminate={
+                            selected?.length > 0 &&
+                            selected?.length < rows?.length
+                          }
+                          checked={
+                            rows?.length > 0 &&
+                            selected?.length === rows?.length
+                          }
+                          color="primary"
+                          onChange={handleSelectAllClick}
+                          inputProps={{ "aria-label": "select all" }}
+                        />
+                      </TableCell>
+                    ) : (
+                      <TableCell
+                        className={classes.tableCellHeader}
+                        align="center"
+                        key={item.id}
+                      >
+                        {item.name} {item?.name2 ? `/ ${item?.name2}` : null}
+                      </TableCell>
+                    )
+                  )}
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows?.map((row, index) => (
-                  <TableRow
-                    key={row?.id}
-                    className={index % 2 === 1 ? classes.darkTableRow : null}
-                  >
-                    {editableTableColumns?.map((item, i) => (
-                      <TableCell
-                        key={i}
-                        className={classes.tableCell}
-                        align="center"
-                      >
-                        {item?.objKey === "approved" ? (
-                          <label className="container">
-                            <input
-                              type="checkbox"
-                              className={classes.checkbox}
-                              defaultChecked={row[item?.objKey]}
-                              onClick={(e) => handleReady(e, row.id)}
-                              name="approved"
+                {rows?.map((row, index) => {
+                  const isItemSelected = selected.indexOf(row.id) !== -1;
+                  const labelId = `enhanced-table-checkbox-${index}`;
+                  return (
+                    <TableRow
+                      key={row?.id}
+                      className={index % 2 === 1 ? classes.darkTableRow : null}
+                    >
+                      {editableTableColumns?.map((item, i) => (
+                        <TableCell
+                          key={i}
+                          className={classes.tableCell}
+                          align="center"
+                        >
+                          {item?.objKey === "approved" ? (
+                            <label className="container">
+                              <td
+                                style={{
+                                  padding: 0,
+                                  borderBottom: "1px solid #e0e0e0",
+                                  pointerEvents:
+                                    row.status === "pending" ? "auto" : "none",
+                                  borderRight: "0.5px solid #E0E0E0",
+                                }}
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  handleCheckBoxClick(e, row.id, row);
+                                }}
+                                onBlur={(e) => {
+                                  e.stopPropagation();
+                                }}
+                                onChange={(e) => {
+                                  e.stopPropagation();
+                                }}
+                              >
+                                <Checkbox
+                                  checked={isItemSelected}
+                                  disabled={
+                                    !(
+                                      !!row.supplier &&
+                                      !!row.type &&
+                                      !!row.color &&
+                                      !!row.length
+                                    )
+                                  }
+                                  color="primary"
+                                  inputProps={{ "aria-labelledby": labelId }}
+                                />
+                              </td>
+                            </label>
+                          ) : item?.objKey === "personalization" &&
+                            (row[item?.objKey] === null ||
+                              row[item?.objKey] === "") ? (
+                            "None"
+                          ) : item?.objKey === "status" ? (
+                            <select
+                              name="status"
+                              id="status"
+                              className={classes.select}
+                              value={row[item?.objKey]}
+                              onChange={(e) => handleOptionChange(e, row.id)}
+                            >
+                              <option value="awaiting">awaiting</option>
+                              <option value="pending">pending</option>
+                              <option value="in_progress">processing</option>
+                              <option value="ready">ready</option>
+                              <option value="in_transit">in_transit</option>
+                              <option value="repeat">repeat</option>
+                              <option value="shipped">shipped</option>
+                              <option value="cancelled">cancelled</option>
+                              <option value="follow_up">follow_up</option>
+                            </select>
+                          ) : item?.objKey === "supplier" ? (
+                            <select
+                              name="supplier"
+                              id="supplier"
+                              className={classes.select}
+                              value={row[item?.objKey]}
+                              onChange={(e) => handleOptionChange(e, row.id)}
+                            >
+                              <option value="asya">asya</option>
+                              <option value="beyazit">beyazit</option>
+                              {/* <option value="stok">stok</option> */}
+                            </select>
+                          ) : item?.objKey === "created_date" ? (
+                            moment(row[item?.objKey]).format(
+                              "MM-DD-YY HH:mm"
+                            ) === "Invalid date" ? (
+                              row[item?.objKey]
+                            ) : (
+                              moment
+                                .utc(row[item?.objKey])
+                                .local()
+                                .format("MM-DD-YY HH:mm")
+                            )
+                          ) : item?.objKey === "type" ||
+                            item?.objKey === "length" ||
+                            item?.objKey === "color" ||
+                            item?.objKey === "qty" ||
+                            item?.objKey === "size" ||
+                            item?.objKey === "start" ||
+                            item?.objKey === "explanation" ||
+                            item?.objKey === "space" ||
+                            item?.objKey === "note" ? (
+                            <EditableCell
+                              {...{
+                                row,
+                                name: item?.objKey,
+                                onChange,
+                              }}
                             />
-                          </label>
-                        ) : item?.objKey === "personalization" &&
-                          (row[item?.objKey] === null ||
-                            row[item?.objKey] === "") ? (
-                          "None"
-                        ) : item?.objKey === "status" ? (
-                          <select
-                            name="status"
-                            id="status"
-                            className={classes.select}
-                            value={row[item?.objKey]}
-                            onChange={(e) => handleOptionChange(e, row.id)}
-                          >
-                            <option value="awaiting">awaiting</option>
-                            <option value="pending">pending</option>
-                            <option value="in_progress">processing</option>
-                            <option value="ready">ready</option>
-                            <option value="in_transit">in_transit</option>
-                            <option value="repeat">repeat</option>
-                            <option value="shipped">shipped</option>
-                            <option value="cancelled">cancelled</option>
-                            <option value="follow_up">follow_up</option>
-                          </select>
-                        ) : item?.objKey === "supplier" ? (
-                          <select
-                            name="supplier"
-                            id="supplier"
-                            className={classes.select}
-                            value={row[item?.objKey]}
-                            onChange={(e) => handleOptionChange(e, row.id)}
-                          >
-                            <option value="asya">asya</option>
-                            <option value="beyazit">beyazit</option>
-                            {/* <option value="stok">stok</option> */}
-                          </select>
-                        ) : item?.objKey === "created_date" ? (
-                          moment(row[item?.objKey]).format("MM-DD-YY HH:mm") ===
-                          "Invalid date" ? (
-                            row[item?.objKey]
+                          ) : item?.name === "No" ? (
+                            <Link to={`/orders/${row[item?.objKey]}`}>
+                              {row[item?.objKey]}
+                            </Link>
                           ) : (
-                            moment
-                              .utc(row[item?.objKey])
-                              .local()
-                              .format("MM-DD-YY HH:mm")
-                          )
-                        ) : item?.objKey === "type" ||
-                          item?.objKey === "length" ||
-                          item?.objKey === "color" ||
-                          item?.objKey === "qty" ||
-                          item?.objKey === "size" ||
-                          item?.objKey === "start" ||
-                          item?.objKey === "explanation" ||
-                          item?.objKey === "space" ||
-                          item?.objKey === "note" ? (
-                          <EditableCell
-                            {...{
-                              row,
-                              name: item?.objKey,
-                              onChange,
-                            }}
-                          />
-                        ) : item?.name === "No" ? (
-                          <Link to={`/orders/${row[item?.objKey]}`}>
-                            {row[item?.objKey]}
-                          </Link>
-                        ) : (
-                          row[item?.objKey]
-                        )}
-                        {item?.objKey2 ? (
-                          <div>
-                            <br /> {row[item?.objKey2]}
-                          </div>
-                        ) : null}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))}
+                            row[item?.objKey]
+                          )}
+                          {item?.objKey2 ? (
+                            <div>
+                              <br /> {row[item?.objKey2]}
+                            </div>
+                          ) : null}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  );
+                })}
               </TableBody>
             </Table>
           </TableContainer>
